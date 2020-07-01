ccb498262779201b9306941532581dcbf5d2c641
Santiago Palacios <palaciossantiago@gmail.com>
2020-07-01 20:51:38 -0300

    Add uri navbar

--- src/practico_mysql/navBar.php
+++ src/practico_mysql/navBar.php
@@ -4,8 +4,8 @@ include_once "conection.php";
 
 $conexion= Conection();
 
-    $result = $conexion->query("SELECT name FROM links;");
-    $cant = mysqli_num_rows($result);    
+$result = $conexion->query("SELECT name, uri FROM links;");
+$cant = mysqli_num_rows($result);
        
 ?>
 
@@ -18,22 +18,22 @@ $conexion= Conection();
     <title>Productos</title>
 </head>
 <body>
-<!-- NavBar -->
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <ul class="navbar-nav">
-    <?php
-        while($fila = mysqli_fetch_array($result)) {
-            echo '<li class="nav-item active"><a class="nav-link" href="#" >'.$fila['name'].'</a></li>';
-        }        
-    ?>
-  </ul>
+    <!-- NavBar -->
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <ul class="navbar-nav">
+        <?php while($fila = mysqli_fetch_array($result)) : ?>
+          <li class="nav-item active">
+              <?php echo '<a class="nav-link" href="'.$fila['uri'].'" >'.$fila['name'].'</a>'; ?>
+          </li>
+        <?php endwhile;?>
+      </ul>
+    </nav>
+    <!-- End NavBar -->
 
-
-    
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 </body>
 </html>
